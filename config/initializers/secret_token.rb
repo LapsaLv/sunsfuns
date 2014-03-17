2be0e85e06312cb884b43224e37163ef18657ffe
Didzis <didzis@ThinkPad.(none)>
2014-03-17 20:00:19 +0200

    HELLLLLO

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sunsfuns::Application.config.secret_key_base = '8f85dccf12954bfead87fe6209fa16c6477c869a3f3bf0e082ea9802f8c483904d82471017f31f1f510d5d61aabfa5bf6df65e2b8f68665b6d309c9342b7dbec'
